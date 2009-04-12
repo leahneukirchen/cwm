8a490fc270f961a30dadd8b0cb29c09d88c5554d
okan <okan>
2009-04-12 23:51:10 +0000

    we include sys/param.h, so remove sys/types.h; sort while here.
    ok oga@

--- headers.h
+++ headers.h
@@ -21,36 +21,33 @@
 #ifndef _CALMWM_HEADERS_H_
 #define _CALMWM_HEADERS_H_
 
-#include <sys/types.h>
-
-#include <sys/queue.h>
-#include <sys/wait.h>
 #include <sys/param.h>
+#include <sys/queue.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 
 #include <assert.h>
-#include <stdlib.h>
-#include <string.h>
-#include <signal.h>
+#include <ctype.h>
+#include <err.h>
+#include <errno.h>
 #include <dirent.h>
 #include <getopt.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
-#include <errno.h>
 #include <unistd.h>
-#include <ctype.h>
 
+#include <X11/Intrinsic.h>
+#include <X11/Xatom.h>
+#include <X11/Xft/Xft.h>
+#include <X11/Xlib.h>
+#include <X11/Xos.h>
+#include <X11/Xproto.h>
+#include <X11/Xutil.h>
 #include <X11/cursorfont.h>
 #include <X11/extensions/Xinerama.h>
 #include <X11/extensions/Xrandr.h>
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
 #include <X11/keysym.h>
-#include <X11/Xatom.h>
-#include <X11/Xproto.h>
-#include <X11/Intrinsic.h>
-#include <X11/Xos.h>
-#include <X11/Xft/Xft.h>
-
-#include <err.h>
 
 #endif /* _CALMWM_HEADERS_H_ */
