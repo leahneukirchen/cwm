6296efadaa6bcc09068030bec4cb01904ca1a010
okan <okan>
2013-04-12 14:46:30 +0000

    push Screenq into screen_init

--- calmwm.h
+++ calmwm.h
@@ -375,7 +375,7 @@ void			 search_print_client(struct menu *, int);
 
 struct geom		 screen_find_xinerama(struct screen_ctx *, int, int);
 struct screen_ctx	*screen_fromroot(Window);
-void			 screen_init(struct screen_ctx *, u_int);
+void			 screen_init(u_int);
 void			 screen_update_geometry(struct screen_ctx *);
 void			 screen_updatestackingorder(struct screen_ctx *);
 
