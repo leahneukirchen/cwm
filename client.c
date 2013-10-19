a70b2d81af54121eb080d7e9f149b892f78a0b67
okan <okan>
2013-10-19 00:24:54 +0000

    For clients that support WM_TAKE_FOCUS in their WM_PROTOCOLS property, send a ClientMessage event.

--- client.c
+++ client.c
@@ -213,6 +213,8 @@ client_setactive(struct client_ctx *cc, int fg)
 		XInstallColormap(X_Dpy, cc->colormap);
 		XSetInputFocus(X_Dpy, cc->win,
 		    RevertToPointerRoot, CurrentTime);
+		if (cc->xproto & _WM_TAKE_FOCUS)
+			client_msg(cc, cwmh[WM_TAKE_FOCUS]);
 		conf_grab_mouse(cc->win);
 		/*
 		 * If we're in the middle of alt-tabbing, don't change
