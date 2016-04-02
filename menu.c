a889f32af64bc8be266431ada778d55cffb238aa
Christian Neukirchen <chneukirchen@gmail.com>
2016-04-02 17:21:04 +0200

    menu: menuq_add: avoid passing null pointers to vsnprintf.

--- menu.c
+++ menu.c
@@ -601,7 +601,7 @@ menuq_add(struct menu_q *mq, void *ctx, const char *fmt, ...)
 	mi->ctx = ctx;
 
 	va_start(ap, fmt);
-	(void)vsnprintf(mi->text, sizeof(mi->text), fmt, ap);
+	(void)vsnprintf(mi->text, sizeof(mi->text), fmt ? fmt : "", ap);
 	va_end(ap);
 
 	TAILQ_INSERT_TAIL(mq, mi, entry);
