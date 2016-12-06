cf4c1879e52449eb6b921c876e79fc854d079434
okan <okan>
2016-12-06 15:03:13 +0000

    'window-search' is spelled 'menu-window'; the former snuck in during the conversion('menu-window' already existed and was properlly documented); found the hard way by sthen@ while trying to convert.

--- conf.c
+++ conf.c
@@ -63,7 +63,6 @@ static const struct {
 	{ "window-menu-label", kbfunc_menu_client_label, CWM_CONTEXT_CC, {0} },
 	{ "window-lower", kbfunc_client_lower, CWM_CONTEXT_CC, {0} },
 	{ "window-raise", kbfunc_client_raise, CWM_CONTEXT_CC, {0} },
-	{ "window-search", kbfunc_menu_client, CWM_CONTEXT_SC, {0} },
 	{ "window-hide", kbfunc_client_hide, CWM_CONTEXT_CC, {0} },
 	{ "window-delete", kbfunc_client_delete, CWM_CONTEXT_CC, {0} },
 	{ "window-htile", kbfunc_client_htile, CWM_CONTEXT_CC, {0} },
